f17390d7a5113b1c16c868a98751a3b2c78ff5af
Enjam Siddharth Reddy <64941292+SiddharthReddyEnjam@users.noreply.github.com>
2024-01-13 13:24:27 -0600

    added a project

--- client/src/data/projects.js
+++ client/src/data/projects.js
@@ -1,5 +1,6 @@
 // Import images at the top of your React component file
 import handImage from '../images/projects/MoCap/hand.png'; // Adjust the path as necessary
+import countriesLogo from '../images/projects/COUNTRIESOW/countries_main.png';
 import homsLogo from '../images/projects/HOMS/homs-logo.jpg'; // Adjust the path as necessary
 import bloodGram from '../images/projects/BLOOD/BloodGram.png'; // Adjust the path as necessary
 import omniLogo from '../images/projects/OMNI/omni-logo.png'; // Adjust the path as necessary
@@ -13,7 +14,7 @@ const projects = [
       'Capturing of motion in hand postures with a camera and training a machine learning models based of the inputs of 14 users. Here worked with many machine learning models and finally the best outcome is given by deep learning models with a good accuracy.',
     langs: [
       { lang: 'python', color: 'orange' },
-      { lang: 'numpy', color: 'red' },
+      { lang: 'numpy', color: '#840B23' },
       { lang: 'pandas', color: 'green' },
       { lang: 'SQL', color: 'purple' },
     ],
@@ -26,6 +27,20 @@ const projects = [
   },
   {
     id: 2,
+    imgs: countriesLogo,
+    name: 'Countries of the World',
+    description:
+      'Using Tableau to analyze and visualize key global indicators, including economic growth, health, migration, climate impact on agriculture, and the role of technology in education and economy. ',
+    langs: [
+      { lang: 'python', color: 'orange' },
+      { lang: 'numpy', color: '#840B23' },
+      { lang: 'pandas', color: 'green' },
+    ],
+    tools: [{ tool: 'Tableau', color: 'grey' }],
+    details: 'From MCHP',
+  },
+  {
+    id: 3,
     imgs: homsLogo,
     name: 'Hostel Outing Management System',
     description:
@@ -42,7 +57,7 @@ const projects = [
     details: 'From HOMS',
   },
   {
-    id: 3,
+    id: 4,
     imgs: bloodGram,
     name: 'BloodGram',
     description:
@@ -56,7 +71,7 @@ const projects = [
     details: 'Based on Blood Donation',
   },
   {
-    id: 4,
+    id: 5,
     imgs: omniLogo,
     name: 'Omnifood',
     description:
